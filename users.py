dc783f4231c4f1410c908ea9625cb093c3b8665b
Roosa Huttunen <huttunenroosa00@gmail.com>
2022-04-20 15:01:03 +0300

    more structure

---
+++ users.py
@@ -0,0 +1,10 @@
+from db import db
+from flask import session
+from werkzeug.security import check_password_hash, generate_password_hash
+
+def login(username, password):
+    pass
+
+def register(username, password, role):
+    pass
+
