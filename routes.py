4b3ced692d045b9ba68d2268d3b442f1d9d56ebe
Roosa Huttunen <huttunenroosa00@gmail.com>
2022-04-24 16:13:04 +0300

    add register and login

--- routes.py
+++ routes.py
@@ -1,6 +1,49 @@
 from app import app
 from flask import render_template, request, redirect
+import users
 
-@app.route("/")
+
+@app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template('index.html')
+
+
+@app.route('/register', methods=['get', 'post'])
+def reqister():
+    if request.method == 'GET':
+        return render_template('register.html')
+
+    if request.method == 'POST':
+        username = request.form['username']
+        if len(username) > 25:
+            return render_template('error.html', message='The username is too long')
+        if len(username) < 8:
+            return render_template('error.html', message='Username is too short')
+
+        password1 = request.form['password1']
+        password2 = request.form['password2']
+        if password1 != password2:
+            return render_template('error.html', message='The passwords are not the same')
+        if len(password1) < 8:
+            return render_template('error.html', message='Password is too short')
+
+        role = request.form['role']
+        if role not in ['1','2']:
+            return render_template('error.html', message='Unknown user')
+
+        if users.register(username, password1, role) is False:
+            return render_template('error.html', message='The registration was unsuccesful, try a different username')
+
+        return redirect('/')
+
+@app.route('/login', methods=['get', 'post'])
+def login():
+    if request.method == 'GET':
+        return render_template('login.html')
+    
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if not users.login(username,password):
+            return render_template('error.html', message='Wrong username or password')
+        return redirect('/')
--- users.py
+++ users.py
@@ -1,10 +1,44 @@
 from db import db
-from flask import session
+from flask import session, abort
 from werkzeug.security import check_password_hash, generate_password_hash
 
+
 def login(username, password):
-    pass
+    sql = 'SELECT id, role, password, username FROM users WHERE username=:username'
+    result = db.session.execute(sql, {'username': username})
+    user = result.fetchone()
+    if not user:
+        return False
+    else:
+        if check_password_hash(user[2], password):
+            session['user_id'] = user[0]
+            session['user_role'] = user[1]
+            session['user_name'] = user[3]
+            return True
+
 
 def register(username, password, role):
-    pass
+    hash_value = generate_password_hash(password)
+    try:
+        sql = 'INSERT INTO users (username, password, role) VALUES (:username, :password, :role)'
+        db.session.execute(
+            sql, {'username': username, 'password': hash_value, 'role': role})
+        db.session.commit()
+    except:
+        return False
+    return login(username, password)
 
+
+def user_id():
+    return session.get('user_id', 0)
+
+
+def logout():
+    del session['user_id']
+    del session['user_role']
+    del session['user_name']
+
+
+def require_role(role):
+    if role != session.get('user_role', 0):
+        abort(403)
